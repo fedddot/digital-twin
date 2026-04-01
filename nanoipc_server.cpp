a66d8a73e8bfe949aa2d2739aa395a4b48ce7d5d
fedddot <idanrommm@gmail.com>
2026-04-01 12:58:15 +0000

    refactor: remove nanoipc_server_lib and associated source file from CMakeLists

--- nanoipc_server.cpp
+++
@@ -1,7 +0,0 @@
-#include "nanoipc_server.hpp"
-
-#include <string>
-
-// Explicit template instantiation for NanoIpcServer with std::string request and int response
-template class nanoipc::NanoIpcServer<std::string, int>;
-
