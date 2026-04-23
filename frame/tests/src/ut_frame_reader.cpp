d17ffa44be4ea69adc03361c79d168239cea9f24
fedddot <idanrommm@gmail.com>
2026-04-23 06:39:46 +0000

    refactor: improve formatting and organization in devcontainer.json and ut_frame_reader.cpp

--- frame/tests/src/ut_frame_reader.cpp
+++ frame/tests/src/ut_frame_reader.cpp
@@ -1,14 +1,13 @@
-#include "gtest/gtest.h"
-
 #include "frame_reader.hpp"
 #include "ring_buffer.hpp"
+#include "gtest/gtest.h"
 
 using namespace nanoipc;
 
 TEST(ut_frame_reader, ctor) {
-	// WHEN
-	RingBuffer<10> ring_buffer;
+    // WHEN
+    RingBuffer<10> ring_buffer;
 
-	// THEN
-	ASSERT_NO_THROW(FrameReader reader(&ring_buffer));
+    // THEN
+    ASSERT_NO_THROW(FrameReader reader(&ring_buffer));
 }
